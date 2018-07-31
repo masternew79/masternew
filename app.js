a91ef254ecb72674386f41d920a029f5f72a07f1
MasterNew <tanhoangle1996@gmail.com>
2018-07-31 13:23:14 +0700

    fix cors

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
-        'Access-Controll-Allow-Headers', 
+        'Access-Control-Allow-Headers', 
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     if( req.method === 'OPTION') {
-        res.header('Access-Controll-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
     next();
