6d30a3f71bff50c7e2a6026476973ce23a56d5b5
MasterNew <tanhoangle1996@gmail.com>
2018-08-08 10:25:36 +0700

    fix logging

--- startup/logging.js
+++ startup/logging.js
@@ -4,12 +4,10 @@ require('express-async-errors');
 module.exports = function() {
     process.on('uncaughtException', ex => {
         winston.error(ex.message, ex);
-        process.exit(1);
     });
     
     process.on('unhandledRejection', ex => {
         winston.error(ex.message, ex);
-        process.exit(1);
     });
     winston.add(new winston.transports.Console());
     winston.add(new winston.transports.File({ filename: 'logfile.log' }));
