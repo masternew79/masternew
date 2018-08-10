baba509c5a3a268a62ca80d0f0db9e11bcbb8756
MasterNew <tanhoangle1996@gmail.com>
2018-08-10 13:53:40 +0700

    fix cors

--- startup/cors.js
+++ startup/cors.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
             'Access-Control-Allow-Headers', 
             'Origin, X-Requested-With, Content-Type, Accept, Authorization'
         );
-        if( req.method === 'OPTION') {
+        if( req.method == 'OPTIONS') {
             res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
             return res.status(200).json({});
         }
